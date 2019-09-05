8edb308b9737b7d02b52f70e103cd7aa686d13b8
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-09-05 09:41:44 -0500

    corrected fetch call; Eventbrite api up and running

--- js/api-eventbrite.js
+++ js/api-eventbrite.js
@@ -1,11 +1,10 @@
 
-fetch("https://www.eventbriteapi.com/v3/events/search/?q=parenting&location.address=nashville&token=HHGEDIXLHO35AWYSVFRK"), {
+fetch("https://www.eventbriteapi.com/v3/events/search/?q=parenting&location.address=nashville&token=HHGEDIXLHO35AWYSVFRK", 
+      {
   "headers": {
+      "Authorization": "Bearer HHGEDIXLHO35AWYSVFRK",
       "Accept": "application/json"
-  }
-  .then(data => data.json())
-  .then(parsedData => {
-      console.table(parsedData)
-  })
-}
-
+    }
+  
+  }).then(data => data.json())
+  .then(parsedData => console.table(parsedData))
