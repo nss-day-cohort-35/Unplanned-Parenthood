b0a5cbaa09ae85c31590dbd6439be6ae6b92dd05
Mark McCann <mjmccann12@me.com>
2019-09-05 13:29:02 -0500

    editted api

--- js/api-concerts.js
+++ js/api-concerts.js
@@ -1,5 +1,5 @@
-fetch("https://app.ticketmaster.com/discovery/v1/events.json?apikey=c5nNRGJ3HT9cjlyJiIc9YtjOrc3Tt4IV")
+fetch(`https://app.ticketmaster.com/discovery/v2/events/?keyword=&city=nashville&apikey=c5nNRGJ3HT9cjlyJiIc9YtjOrc3Tt4IV`)
     .then(concerts => concerts.json())
     .then(parsedConcerts => {
-        console.table(parsedConcerts)
+    console.table(parsedConcerts)
     })
