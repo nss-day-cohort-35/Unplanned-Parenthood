148bd42682ea22250e24cc93272afb40af2267af
QuinWithOneN <jquinsmyth@gmail.com>
2019-09-05 17:40:58 -0500

    added parks to DOM Look at you GO.

--- js/parks.js
+++ js/parks.js
@@ -1,5 +1,22 @@
-fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=aH0AJvaMzg4KccAFcjktXYfIe")
-    .then(data => data.json())
-    .then(parsedData => {
-        console.log(parsedData)
-        
+/* parkSearch is a variable that runs a function (return fetch). Fetch accesses the API data and converts it into a format that JavaScript can read (parkData => parkData.json()). That readable data is then run through the function "addParkToDom" in order to be displayed on your webpage. */
+const parkSearch = (searchTerm) => {
+    return fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=aH0AJvaMzg4KccAFcjktXYfIe")
+      .then(parkData => parkData.json())
+      .then(parsedParkData => {
+          addParkToDom(parsedParkData)
+          console.log(parsedParkData)
+      }   )
+   } 
+   /* pulls name from the array*/
+   const parksNashville = (parksInNash) => {
+      return `<p>${parksInNash.park_name}</p>`
+  } 
+  /*display restaurant names to the dom*/
+   function addParkToDom(parksInNash) {
+  parksInNash.forEach(parkObj => {
+      parkContainer.innerHTML += parksNashville(parkObj);
+        });
+    }
+   const parkContainer = document.querySelector("#nashParks");
+  /*calls function that contains fetch*/
+   parkSearch();
