8ba2382d17cbd3e61cbcce72760111bdd9a9366a
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-09-05 10:34:31 -0500

    committing latest changes

--- js/api-eventbrite.js
+++ js/api-eventbrite.js
@@ -1,5 +1,5 @@
 
-fetch("https://www.eventbriteapi.com/v3/events/search/?q=parenting&location.address=nashville&token=HHGEDIXLHO35AWYSVFRK", 
+fetch(`https://www.eventbriteapi.com/v3/events/search/?q=parenting&location.address=nashville&token=HHGEDIXLHO35AWYSVFRK`, 
       {
   "headers": {
       "Authorization": "Bearer HHGEDIXLHO35AWYSVFRK",
