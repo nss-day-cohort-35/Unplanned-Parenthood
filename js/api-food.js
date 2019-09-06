8fad934445a99cef17cf67a69b2e651a69bffbe5
lauren maxwell <laurenelizamax@icloud.com>
2019-09-06 14:25:03 -0500

    event listener search works

--- js/api-food.js
+++ js/api-food.js
@@ -1,3 +1,14 @@
+/* restaurant event listener*/
+document.querySelector("#restaurantsButton").addEventListener("click", event => {
+    let userInput = document.querySelector("#restaurantInput").value
+    restaurantSearch(userInput);
+})
+/*display restaurant names to the dom*/
+function addFoodToDom(foodInNash) {
+    foodInNash.forEach(restaurantObj => {
+        restoContainer.innerHTML += restaurantsNashville(restaurantObj);
+    } ) ; }
+/* fetch call*/
  const restaurantSearch = (searchTerm) => {
   return fetch(`https://developers.zomato.com/api/v2.1/search?q=${searchTerm}&entity_id=1138&entity_type=city&start=first&sort=rating&apikey=98b3e8584106245a199e8c4987ab27b0`)
     .then(restaurants => restaurants.json())
@@ -10,19 +21,11 @@
  const restaurantsNashville = (foodInNash) => {
     return `<p>${foodInNash.restaurant.name}</p>`
 } 
-/*display restaurant names to the dom*/
- function addFoodToDom(foodInNash) {
-foodInNash.forEach(restaurantObj => {
-    restoContainer.innerHTML += restaurantsNashville(restaurantObj);
-});}
+
  const restoContainer = document.querySelector("#nashFood");
 /*calls function that contains fetch*/
- restaurantSearch("");
  
-//  document.querySelector("#restaurantsButton").addEventListener("click"), event => {
-//     restaurantSearch = document.querySelector(restuarantsNashville).value
 
-//  }
     
 
 
