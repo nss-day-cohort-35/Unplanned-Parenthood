7e78bd136f470a175c023ea66a663a6f479bc273
Leigh Rogers <leighrogers@Leighs-MacBook-Pro.local>
2019-09-06 14:23:41 -0500

    got event listener search to work on meetups; yay

--- js/api-eventbrite.js
+++ js/api-eventbrite.js
@@ -1,4 +1,23 @@
+//adds the event listener and query selector for ID to trigger the click event. Calls the search function in the event listener.
+document.querySelector("#meetupButton").addEventListener("click", event => {
+    let userInput = document.querySelector("#meetupInput").value
+    meetupSearch(userInput);
+}) 
 
+/*display restaurant names to the dom*/
+function addMeetupToDom(meetupInNash) {
+    meetupInNash.forEach(meetupObj => {
+        meetupContainer.innerHTML += meetupsNashville(meetupObj);
+    });}
+
+/* pulls name from the array*/
+const meetupsNashville = (meetupInNash) => {
+    return `<p>${meetupInNash.name.text}</p>`
+} 
+
+const meetupContainer = document.querySelector("#nashMeetups");
+
+/*function fetches search*/
 const meetupSearch = (searchTerm) => {
 return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchTerm}&location.address=nashville&token=HHGEDIXLHO35AWYSVFRK`, 
       {
@@ -6,23 +25,10 @@ return fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${searchTerm}&lo
         "Authorization": "Bearer HHGEDIXLHO35AWYSVFRK",
         "Accept": "application/json"
     }
-
     }).then(meetupData => meetupData.json())
     .then(parsedMeetupData => {
         addMeetupToDom(parsedMeetupData.events)
         console.table(parsedMeetupData)
     })
 }
- 
-/* pulls name from the array*/
-const meetupsNashville = (meetupInNash) => {
-    return `<p>${meetupInNash.name.text}</p>`
-} 
-/*display restaurant names to the dom*/
-function addMeetupToDom(meetupInNash) {
-meetupInNash.forEach(meetupObj => {
-    meetupContainer.innerHTML += meetupsNashville(meetupObj);
-});}
-const meetupContainer = document.querySelector("#nashMeetups");
-/*calls function that contains fetch*/
-meetupSearch("parenting");
+
