0d7b7ec80b62787dc30b610111756fe3287fea14
Mark McCann <mjmccann12@me.com>
2019-09-06 14:25:18 -0500

    added event listener

--- js/api-concerts.js
+++ js/api-concerts.js
@@ -1,3 +1,12 @@
+document.querySelector("#concertsButton").addEventListener("click", event => {
+    let userInput = document.querySelector("#concertsInput").value
+    concertsSearch(userInput);
+})
+/*display restaurant names to the dom*/
+function addConcertsToDom(concertsInNash) {
+    concertsInNash.forEach(concertsObj => {
+        concertsContainer.innerHTML += concertsNashville(concertsObj);
+    });}
 const concertsSearch = (searchTerm) => {
     return fetch(`https://app.ticketmaster.com/discovery/v2/events/?keyword=${searchTerm}&city=nashville&apikey=c5nNRGJ3HT9cjlyJiIc9YtjOrc3Tt4IV`)
     .then(concerts => concerts.json())
@@ -9,11 +18,5 @@ const concertsSearch = (searchTerm) => {
 const concertsNashville = (concertsInNash) => {
     return `<p>${concertsInNash.name}</p>`
 } 
-/*display restaurant names to the dom*/
- function addConcertsToDom(concertsInNash) {
-concertsInNash.forEach(concertsObj => {
-    concertsContainer.innerHTML += concertsNashville(concertsObj);
-});}
  const concertsContainer = document.querySelector("#concertsNash");
 /*calls function that contains fetch*/
- concertsSearch("dion");
