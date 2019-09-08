226d0339f9d7b44950afd50a4f13cee6ce35a4af
lauren maxwell <laurenelizamax@icloud.com>
2019-09-08 17:25:35 -0500

    edited save button

--- js/api-food.js
+++ js/api-food.js
@@ -3,13 +3,22 @@ document.querySelector("#restaurantsButton").addEventListener("click", event => 
     let userInput = document.querySelector("#restaurantInput").value
     restaurantSearch(userInput);
 })
+
+
+
+// const restResults = () => {
+//     for (let i = 0; i < restaurantSearch.length; i++){
+   
+// }
+// }
+
 /*display restaurant names to the dom*/
 function addFoodToDom(foodInNash) {
     foodInNash.forEach(restaurantObj => {
         restoContainer.innerHTML += restaurantsNashville(restaurantObj);
     } ) ; }
-/* fetch call*/
- const restaurantSearch = (searchTerm) => {
+/*calls function that contains fetch*/
+const restaurantSearch = (searchTerm) => {
   return fetch(`https://developers.zomato.com/api/v2.1/search?q=${searchTerm}&entity_id=1138&entity_type=city&start=first&sort=rating&apikey=98b3e8584106245a199e8c4987ab27b0`)
     .then(restaurants => restaurants.json())
     .then(parsedRestaurants => {
@@ -17,20 +26,16 @@ function addFoodToDom(foodInNash) {
         console.log(parsedRestaurants)
     }   )
  } 
+ const restoContainer = document.querySelector("#nashFood");
+
  /* pulls name from the array*/
  const restaurantsNashville = (foodInNash) => {
-    return `<h4>${foodInNash.restaurant.name}</h4>
+    return `  <h4>${foodInNash.restaurant.name}</h4>
     <p>${foodInNash.restaurant.location.address}</p>
-    <p>${foodInNash.restaurant.phone_numbers}</p>`
-
-} 
-
- const restoContainer = document.querySelector("#nashFood");
-/*calls function that contains fetch*/
- 
-
-    
-
-
+    <p>${foodInNash.restaurant.phone_numbers}</p>
+    <button type="button" id="restaurantSaveButton">${foodInNash.restaurant="Save"}</button> 
+    `
+        // creates save button
+ }
 
 
